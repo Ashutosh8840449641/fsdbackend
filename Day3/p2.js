0ec8c6e0ada4d33a2467c2467a72f26c4aedb276
Ashutosh8840449641 <ashutoshyadav3012@gmail.com>
2025-02-27 15:41:18 +0530

    DAy3

---
+++ Day3/p2.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200,{'Content-Type':'text/html'});
+    if(req.url==='/setdata' && req.method==='POST')
+    {
+        let body='';
+        req.on('data', chunk=>{
+            body+=chunk;
+        });
+        req.on('end', ()=>{
+            let data=JSON.parse(body);
+            console.log(data);
+            res.end(JSON.stringify({message: 'Data received'}));
+        });
+    }});
+
+server.listen((9000),(err) =>{
+    if(err)
+        console.log("Error starting error",err);
+    else
+        console.log("server sterted on port 9000")
+});
